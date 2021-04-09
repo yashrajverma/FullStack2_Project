3a3498aac3b39179d6b6a7f5f4e344f3c63bc15a
yashrajverma <yashrajverma798@gmail.com>
2021-04-09 15:23:09 +0530

    Completed

---
+++ server/server/config/dev.js
@@ -0,0 +1,8 @@
+
+module.exports={
+    MONGOURI:"mongodb+srv://yashraj_verma:FZsj0gbGUb8K8Plk@cluster0.1pvwf.mongodb.net/instagram?retryWrites=true&w=majority"
+    ,JWT_SECRET:"dkjjfakjs@34"
+
+}
+
+//FZsj0gbGUb8K8Plk
