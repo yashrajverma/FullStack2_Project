c0a984562c3795cb7b907df57a10c25a63eadae2
yashrajverma <yashrajverma798@gmail.com>
2021-04-13 00:25:22 +0530

    Completed

--- .history/app_20210412234604.js
+++ .history/app_20210412234604.js
@@ -23,7 +23,6 @@ app.use(express.json())
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
 app.use(require('./routes/user'))
-
 if(process.env.NODE_ENV=="production"){
     app.use(express.static("client/build"))
     const path =require("path")
